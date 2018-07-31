f110cdbab36b327ef7672d716346a0de02476aab
Sylvain Bertrand <sbertrand@ihmc.us>
2018-07-31 10:37:12 +0200

    Added a test when unpinning the floating joint.

--- src/test/java/us/ihmc/simulationconstructionset/physics/featherstone/PinnedFloatingJointTest.java
+++ src/test/java/us/ihmc/simulationconstructionset/physics/featherstone/PinnedFloatingJointTest.java
@@ -31,20 +31,20 @@ public class PinnedFloatingJointTest
    public void testPinnedFloatingJoint() throws Exception, UnreasonableAccelerationException
    {
       Random random = new Random(39847534);
-         double dt = 1.0e-3;
+      double dt = 1.0e-3;
 
       for (int iteration = 0; iteration < 50; iteration++)
       {
          int numberOneDoFJoints = random.nextInt(15) + 1;
          Robot robotA = new Robot("pinnedRobot");
-         FloatingJoint pinnedJoint = new FloatingJoint("pinned", new Vector3D(), robotA);
-         pinnedJoint.setPinned(true);
-         pinnedJoint.setLink(nextLink(random.nextLong()));
-         robotA.addRootJoint(pinnedJoint);
+         FloatingJoint pinnedJointA = new FloatingJoint("pinned", new Vector3D(), robotA);
+         pinnedJointA.setPinned(true);
+         pinnedJointA.setLink(nextLink(random.nextLong()));
+         robotA.addRootJoint(pinnedJointA);
 
-         Robot robotB = new Robot("robotWithFloatingJoint");
+         Robot robotB = new Robot("robotWithoutFloatingJoint");
 
-         Joint parentJointA = pinnedJoint;
+         Joint parentJointA = pinnedJointA;
          Joint parentJointB = null;
 
          OneDegreeOfFreedomJoint[] oneDoFJointsA = new OneDegreeOfFreedomJoint[numberOneDoFJoints];
@@ -72,14 +72,13 @@ public class PinnedFloatingJointTest
             parentJointB = nextJointB;
          }
 
-
          for (int i = 0; i < 100; i++)
          {
             // Make sure that the joint velocity and acceleration are both 
-            pinnedJoint.setVelocity(EuclidCoreRandomTools.nextVector3D(random));
-            pinnedJoint.setAngularVelocityInBody(EuclidCoreRandomTools.nextVector3D(random));
-            pinnedJoint.setAcceleration(EuclidCoreRandomTools.nextVector3D(random));
-            pinnedJoint.setAngularAccelerationInBody(EuclidCoreRandomTools.nextVector3D(random));
+            pinnedJointA.setVelocity(EuclidCoreRandomTools.nextVector3D(random));
+            pinnedJointA.setAngularVelocityInBody(EuclidCoreRandomTools.nextVector3D(random));
+            pinnedJointA.setAcceleration(EuclidCoreRandomTools.nextVector3D(random));
+            pinnedJointA.setAngularAccelerationInBody(EuclidCoreRandomTools.nextVector3D(random));
 
             for (int jointIndex = 0; jointIndex < numberOneDoFJoints; jointIndex++)
             {
@@ -91,24 +90,145 @@ public class PinnedFloatingJointTest
             robotA.doDynamicsAndIntegrate(dt);
             robotB.doDynamicsAndIntegrate(dt);
 
-            assertEquals(0.0, pinnedJoint.getQx().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getQy().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getQz().getValue(), EPSILON);
-            EuclidCoreTestTools.assertQuaternionEquals(new Quaternion(), pinnedJoint.getQuaternion(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getQx().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getQy().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getQz().getValue(), EPSILON);
+            EuclidCoreTestTools.assertQuaternionEquals(new Quaternion(), pinnedJointA.getQuaternion(), EPSILON);
 
-            assertEquals(0.0, pinnedJoint.getQdx().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getQdy().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getQdz().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getAngularVelocityX().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getAngularVelocityY().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getAngularVelocityZ().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getQdx().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getQdy().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getQdz().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getAngularVelocityX().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getAngularVelocityY().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getAngularVelocityZ().getValue(), EPSILON);
 
-            assertEquals(0.0, pinnedJoint.getQddx().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getQddy().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getQddz().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getAngularAccelerationX().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getAngularAccelerationY().getValue(), EPSILON);
-            assertEquals(0.0, pinnedJoint.getAngularAccelerationZ().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getQddx().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getQddy().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getQddz().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getAngularAccelerationX().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getAngularAccelerationY().getValue(), EPSILON);
+            assertEquals(0.0, pinnedJointA.getAngularAccelerationZ().getValue(), EPSILON);
+
+            for (int jointIndex = 0; jointIndex < numberOneDoFJoints; jointIndex++)
+            {
+               assertEquals(oneDoFJointsA[jointIndex].getQ(), oneDoFJointsB[jointIndex].getQ(), EPSILON);
+               assertEquals(oneDoFJointsA[jointIndex].getQD(), oneDoFJointsB[jointIndex].getQD(), EPSILON);
+               assertEquals(oneDoFJointsA[jointIndex].getQDD(), oneDoFJointsB[jointIndex].getQDD(), EPSILON);
+            }
+         }
+      }
+   }
+
+   @Test(timeout = 30000)
+   public void testUnpinningFloatingJoint() throws Exception, UnreasonableAccelerationException
+   {
+      Random random = new Random(39847534);
+      double dt = 1.0e-3;
+
+      for (int iteration = 0; iteration < 50; iteration++)
+      {
+         int numberOneDoFJoints = random.nextInt(15) + 1;
+         Robot robotA = new Robot("unpinningRobot");
+         FloatingJoint floatingJointA = new FloatingJoint("unpinning", new Vector3D(), robotA);
+         floatingJointA.setPinned(true);
+         long rootLinkSeed = random.nextLong();
+         floatingJointA.setLink(nextLink(rootLinkSeed));
+         robotA.addRootJoint(floatingJointA);
+
+         Robot robotB = new Robot("robotWithFloatingJoint");
+         FloatingJoint floatingJointB = new FloatingJoint("unpinning", new Vector3D(), robotB);
+         floatingJointB.setLink(nextLink(rootLinkSeed));
+         robotB.addRootJoint(floatingJointB);
+
+         Joint parentJointA = floatingJointA;
+         Joint parentJointB = floatingJointB;
+
+         OneDegreeOfFreedomJoint[] oneDoFJointsA = new OneDegreeOfFreedomJoint[numberOneDoFJoints];
+         OneDegreeOfFreedomJoint[] oneDoFJointsB = new OneDegreeOfFreedomJoint[numberOneDoFJoints];
+
+         for (int i = 0; i < numberOneDoFJoints; i++)
+         {
+            long seed = random.nextLong();
+
+            OneDegreeOfFreedomJoint nextJointA = nextOneDegreeOfFreedomJoint(seed, robotA);
+            OneDegreeOfFreedomJoint nextJointB = nextOneDegreeOfFreedomJoint(seed, robotB);
+            nextJointA.setLink(nextLink(seed));
+            nextJointB.setLink(nextLink(seed));
+
+            parentJointA.addJoint(nextJointA);
+            parentJointB.addJoint(nextJointB);
+
+            oneDoFJointsA[i] = nextJointA;
+            oneDoFJointsB[i] = nextJointB;
+
+            parentJointA = nextJointA;
+            parentJointB = nextJointB;
+         }
+
+         for (int i = 0; i < 100; i++)
+         { // Run dynamics on the robot A
+            floatingJointA.setVelocity(EuclidCoreRandomTools.nextVector3D(random));
+            floatingJointA.setAngularVelocityInBody(EuclidCoreRandomTools.nextVector3D(random));
+            floatingJointA.setAcceleration(EuclidCoreRandomTools.nextVector3D(random));
+            floatingJointA.setAngularAccelerationInBody(EuclidCoreRandomTools.nextVector3D(random));
+
+            for (int jointIndex = 0; jointIndex < numberOneDoFJoints; jointIndex++)
+            {
+               double tau = random.nextDouble();
+               oneDoFJointsA[jointIndex].setTau(tau);
+            }
+
+            robotA.doDynamicsAndIntegrate(dt);
+         }
+
+         // Copy the robot states A => B
+         floatingJointB.getQx().set(floatingJointA.getQx().getValue());
+         floatingJointB.getQy().set(floatingJointA.getQy().getValue());
+         floatingJointB.getQz().set(floatingJointA.getQz().getValue());
+         floatingJointB.getQuaternionQs().set(floatingJointA.getQuaternionQs().getValue());
+         floatingJointB.getQuaternionQx().set(floatingJointA.getQuaternionQx().getValue());
+         floatingJointB.getQuaternionQy().set(floatingJointA.getQuaternionQy().getValue());
+         floatingJointB.getQuaternionQz().set(floatingJointA.getQuaternionQz().getValue());
+
+         for (int jointIndex = 0; jointIndex < numberOneDoFJoints; jointIndex++)
+         {
+            oneDoFJointsB[jointIndex].setQ(oneDoFJointsA[jointIndex].getQ());
+            oneDoFJointsB[jointIndex].setQd(oneDoFJointsA[jointIndex].getQD());
+            oneDoFJointsB[jointIndex].setQdd(oneDoFJointsA[jointIndex].getQDD());
+         }
+
+         floatingJointA.setPinned(false);
+
+         for (int i = 0; i < 100; i++)
+         {
+            for (int jointIndex = 0; jointIndex < numberOneDoFJoints; jointIndex++)
+            {
+               double tau = random.nextDouble();
+               oneDoFJointsA[jointIndex].setTau(tau);
+               oneDoFJointsB[jointIndex].setTau(tau);
+            }
+
+            robotA.doDynamicsAndIntegrate(dt);
+            robotB.doDynamicsAndIntegrate(dt);
+
+            assertEquals(floatingJointB.getQx().getValue(), floatingJointA.getQx().getValue(), EPSILON);
+            assertEquals(floatingJointB.getQy().getValue(), floatingJointA.getQy().getValue(), EPSILON);
+            assertEquals(floatingJointB.getQz().getValue(), floatingJointA.getQz().getValue(), EPSILON);
+            EuclidCoreTestTools.assertQuaternionEquals(floatingJointB.getQuaternion(), floatingJointA.getQuaternion(), EPSILON);
+
+            assertEquals(floatingJointB.getQdx().getValue(), floatingJointA.getQdx().getValue(), EPSILON);
+            assertEquals(floatingJointB.getQdy().getValue(), floatingJointA.getQdy().getValue(), EPSILON);
+            assertEquals(floatingJointB.getQdz().getValue(), floatingJointA.getQdz().getValue(), EPSILON);
+            assertEquals(floatingJointB.getAngularVelocityX().getValue(), floatingJointA.getAngularVelocityX().getValue(), EPSILON);
+            assertEquals(floatingJointB.getAngularVelocityY().getValue(), floatingJointA.getAngularVelocityY().getValue(), EPSILON);
+            assertEquals(floatingJointB.getAngularVelocityZ().getValue(), floatingJointA.getAngularVelocityZ().getValue(), EPSILON);
+
+            assertEquals(floatingJointB.getQddx().getValue(), floatingJointA.getQddx().getValue(), EPSILON);
+            assertEquals(floatingJointB.getQddy().getValue(), floatingJointA.getQddy().getValue(), EPSILON);
+            assertEquals(floatingJointB.getQddz().getValue(), floatingJointA.getQddz().getValue(), EPSILON);
+            assertEquals(floatingJointB.getAngularAccelerationX().getValue(), floatingJointA.getAngularAccelerationX().getValue(), EPSILON);
+            assertEquals(floatingJointB.getAngularAccelerationY().getValue(), floatingJointA.getAngularAccelerationY().getValue(), EPSILON);
+            assertEquals(floatingJointB.getAngularAccelerationZ().getValue(), floatingJointA.getAngularAccelerationZ().getValue(), EPSILON);
 
             for (int jointIndex = 0; jointIndex < numberOneDoFJoints; jointIndex++)
             {
