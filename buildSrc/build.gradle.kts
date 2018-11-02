faa8a82859cb2c9a81be05eb489b24330e020777
Duncan Calvert <dcalvert@ihmc.us>
2018-11-02 13:54:40 -0500

    Add SCS Gradle plugin.

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,51 @@
+import com.gradle.publish.MavenCoordinates
+import com.gradle.publish.PluginConfig
+
+plugins {
+   `java-gradle-plugin`
+   kotlin("jvm") version "1.2.61"
+   id("com.gradle.plugin-publish") version "0.9.9"
+}
+
+group = "us.ihmc"
+version = "0.1"
+
+repositories {
+   mavenCentral()
+   jcenter()
+}
+
+dependencies {
+   compile(gradleApi())
+   compile(kotlin("stdlib"))
+}
+
+gradlePlugin {
+   plugins {
+      register("scsGradlePlugin") {
+         id = project.group as String + "." + project.name
+         displayName = "SCS Gradle Plugin"
+         implementationClass = "us.ihmc.scs.SCSPlugin"
+         description = "Property initialization for IHMC's Simulation Construction Set."
+      }
+   }
+}
+
+pluginBundle {
+   website = "https://github.com/ihmcrobotics/simulation-construction-set"
+   vcsUrl = "https://github.com/ihmcrobotics/simulation-construction-set"
+   description = "Runtime configuration for IHMC's Simulation Construction Set."
+   tags = listOf("scs", "property", "initialization", "ihmc", "robotics", "simulation")
+
+   plugins.register("scsGradlePlugin") {
+      id = project.group as String + "." + project.name
+      version = project.version as String
+      displayName = "SCS Gradle Plugin"
+   }
+
+   mavenCoordinates(closureOf<MavenCoordinates> {
+      groupId = project.group as String
+      artifactId = project.name
+      version = project.version as String
+   })
+}
