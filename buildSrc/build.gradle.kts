c9d3fc78c054cd6b66e1b851d6f7ee511f8f66a0
Duncan Calvert <dcalvert@ihmc.us>
2018-12-13 13:51:22 -0600

    :bookmark: SCS Gradle plugin 0.3. Move sets back to evaluate, and re-set them on command.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "us.ihmc"
-version = "0.2"
+version = "0.3"
 
 repositories {
    mavenCentral()
--- buildSrc/src/main/kotlin/us/ihmc/scs/SCSPlugin.kt
+++ buildSrc/src/main/kotlin/us/ihmc/scs/SCSPlugin.kt
@@ -12,6 +12,7 @@ class SCSPlugin : Plugin<Project>
    var scsVideo = false
    var runningOnCIServer = false
    val javaProperties = hashMapOf<String, String>()
+   lateinit var project: Project
 
    open class SCSExtension(val javaProperties: HashMap<String, String>,
                            val scsPlugin: SCSPlugin)  // in the future, add real fields?
@@ -24,6 +25,8 @@ class SCSPlugin : Plugin<Project>
             javaProperties["create.scs.gui"] = "true"
             javaProperties["java.awt.headless"] = "false"
             javaProperties["show.scs.yographics"] = "true"
+
+            scsPlugin.putProperties()
          }
       }
 
@@ -38,12 +41,16 @@ class SCSPlugin : Plugin<Project>
                javaProperties["create.videos.dir"] = "/opt/ihmc/bamboo-videos"
             else
                javaProperties["create.videos.dir"] = System.getProperty("user.home") + "/bamboo-videos"
+
+            scsPlugin.putProperties()
          }
       }
    }
 
    override fun apply(project: Project)
    {
+      this.project = project
+
       project.properties["scsVideo"].run { if (this != null) scsVideo = (this as String).toBoolean() }
       project.properties["scsShowGui"].run { if (this != null) scsShowGui = (this as String).toBoolean() }
       System.getenv("RUNNING_ON_CONTINUOUS_INTEGRATION_SERVER").run { if (this != null) runningOnCIServer = (this as String).toBoolean() }
@@ -81,32 +88,31 @@ class SCSPlugin : Plugin<Project>
 
       project.extensions.create("scs", SCSExtension::class.java, javaProperties, this)
 
+      putProperties()
+   }
+
+   private fun putProperties()
+   {
       for (allproject in project.allprojects)
       {
          allproject.tasks.withType(JavaExec::class.java) { javaExec ->
-            javaExec.doFirst {
-               // setup properties for all JavaExec tasks
-               javaExec.systemProperties.putAll(javaProperties)
-               allproject.logger.info("[scs] Passing JVM args ${javaExec.systemProperties} to $javaExec")
-            }
+            // setup properties for all JavaExec tasks
+            javaExec.systemProperties.putAll(javaProperties)
+            allproject.logger.info("[scs] Passing JVM args ${javaExec.systemProperties} to $javaExec")
          }
          allproject.tasks.withType(Test::class.java) { test ->
-            test.doFirst {
-               // setup properties for forked test jvms
-               test.systemProperties.putAll(javaProperties)
-               allproject.logger.info("[scs] Passing JVM args ${test.systemProperties} to $test")
-            }
+            // setup properties for forked test jvms
+            test.systemProperties.putAll(javaProperties)
+            allproject.logger.info("[scs] Passing JVM args ${test.systemProperties} to $test")
          }
          allproject.tasks.withType(CreateStartScripts::class.java) { startScripts ->
-            startScripts.doFirst {
-               // setup properties for all start scripts (includes application plugin)
-               val list = arrayListOf<String>()
-               javaProperties.forEach {
-                  list.add("-D${it.key}=${it.value}")
-               }
-               startScripts.defaultJvmOpts = list
-               allproject.logger.info("[scs] Passing JVM args $list to $startScripts")
+            // setup properties for all start scripts (includes application plugin)
+            val list = arrayListOf<String>()
+            javaProperties.forEach {
+               list.add("-D${it.key}=${it.value}")
             }
+            startScripts.defaultJvmOpts = list
+            allproject.logger.info("[scs] Passing JVM args $list to $startScripts")
          }
       }
    }
