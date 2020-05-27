f3fe1ced1e3b582e4e9ba8cda1a4242d08d11f4d
Sylvain Bertrand <sbertrand@ihmc.us>
2020-05-27 16:53:47 -0500

    Fixed test: toString method changed a little for YoFrameTuple3D.

--- src/test/java/us/ihmc/simulationconstructionset/KinematicPointTest.java
+++ src/test/java/us/ihmc/simulationconstructionset/KinematicPointTest.java
@@ -150,9 +150,9 @@ public class KinematicPointTest
       YoFramePoint3D yoPosition = kinematicPoint.getYoPosition();
       String frameName = yoPosition.getReferenceFrame().getName();
 
-      assertEquals("( 0.000,  0.000,  0.000 )-" + frameName, yoPosition.toString());
+      assertEquals("( 0.000,  0.000,  0.000 ) - " + frameName, yoPosition.toString());
       yoPosition.set(new Point3D(5.0, 5.1, 5.2));
-      assertEquals("( 5.000,  5.100,  5.200 )-" + frameName, yoPosition.toString());
+      assertEquals("( 5.000,  5.100,  5.200 ) - " + frameName, yoPosition.toString());
    }
 
    @Test // timeout=300000
@@ -161,9 +161,9 @@ public class KinematicPointTest
       YoFrameVector3D yoVelocity = kinematicPoint.getYoVelocity();
       String frameName = yoVelocity.getReferenceFrame().getName();
 
-      assertEquals("( 0.000,  0.000,  0.000 )-" + frameName, yoVelocity.toString());
+      assertEquals("( 0.000,  0.000,  0.000 ) - " + frameName, yoVelocity.toString());
       yoVelocity.set(new Vector3D(5.0, 5.1, 5.2));
-      assertEquals("( 5.000,  5.100,  5.200 )-" + frameName, yoVelocity.toString());
+      assertEquals("( 5.000,  5.100,  5.200 ) - " + frameName, yoVelocity.toString());
    }
 
    @Test // timeout=300000
