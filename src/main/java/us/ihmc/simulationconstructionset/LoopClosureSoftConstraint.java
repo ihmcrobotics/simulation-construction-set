bd21325fd5281a91bbf9f32866fcb64a246bf79f
Sylvain Bertrand <sbertrand@ihmc.us>
2020-06-02 10:10:57 -0500

    Added YoVariables and option to provide feed-forward terms.

--- src/main/java/us/ihmc/simulationconstructionset/LoopClosureSoftConstraint.java
+++ src/main/java/us/ihmc/simulationconstructionset/LoopClosureSoftConstraint.java
@@ -4,6 +4,7 @@ import us.ihmc.euclid.matrix.Matrix3D;
 import us.ihmc.euclid.matrix.interfaces.Matrix3DReadOnly;
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
 import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 import us.ihmc.yoVariables.variable.YoDouble;
@@ -27,21 +28,28 @@ public class LoopClosureSoftConstraint
    private final ExternalForcePoint constraintB;
    private final YoDouble positionErrorMagnitude;
    private final YoDouble rotationErrorMagnitude;
+   private final YoVector3D positionError;
+   private final YoVector3D rotationError;
+   private final YoVector3D linearVelocityError;
+   private final YoVector3D angularVelocityError;
+
+   private final YoVector3D feedForwardForce;
+   private final YoVector3D feedForwardMoment;
 
    private final Vector3D offsetFromParentJoint = new Vector3D();
    private final Vector3D offsetFromLinkParentJoint = new Vector3D();
    private final Matrix3D constraintForceSubSpace = new Matrix3D();
    private final Matrix3D constraintMomentSubSpace = new Matrix3D();
+   private final Vector3D proportionalTermLinear = new Vector3D();
+   private final Vector3D derivativeTermLinear = new Vector3D();
+   private final Vector3D proportionalTermAngular = new Vector3D();
+   private final Vector3D derivativeTermAngular = new Vector3D();
 
    private Joint parentJoint;
 
-   private final Vector3D positionError = new Vector3D();
    private final Quaternion quaternionA = new Quaternion();
    private final Quaternion quaternionB = new Quaternion();
    private final Quaternion quaternionDifference = new Quaternion();
-   private final Vector3D rotationError = new Vector3D();
-   private final Vector3D linearVelocityError = new Vector3D();
-   private final Vector3D angularVelocityError = new Vector3D();
    private final Vector3D forceA = new Vector3D();
    private final Vector3D momentA = new Vector3D();
 
@@ -111,6 +119,13 @@ public class LoopClosureSoftConstraint
       constraintB = new ExternalForcePoint(name + "B", offsetFromLinkParentJoint, robot);
       positionErrorMagnitude = new YoDouble(name + "PositionErrorMagnitude", registry);
       rotationErrorMagnitude = new YoDouble(name + "RotationErrorMagnitude", registry);
+
+      positionError = new YoVector3D(name + "PositionError", registry);
+      rotationError = new YoVector3D(name + "RotationError", registry);
+      linearVelocityError = new YoVector3D(name + "LinearVelocityError", registry);
+      angularVelocityError = new YoVector3D(name + "AngularVelocityError", registry);
+      feedForwardForce = new YoVector3D(name + "FeedForwardForce", registry);
+      feedForwardMoment = new YoVector3D(name + "FeedForwardMoment", registry);
    }
 
    public void setGains(double proportionalGain, double derivativeGain)
@@ -159,6 +174,7 @@ public class LoopClosureSoftConstraint
       constraintB.getParentJoint().getRotationToWorld(quaternionB);
       // Rotation error in A's local coordinates.
       quaternionDifference.difference(quaternionA, quaternionB); // This the orientation from B to A
+      quaternionDifference.normalizeAndLimitToPi();
       quaternionDifference.getRotationVector(rotationError);
       constraintMomentSubSpace.transform(rotationError);
       rotationErrorMagnitude.set(rotationError.length());
@@ -175,13 +191,21 @@ public class LoopClosureSoftConstraint
       parentJoint.transformToNext.inverseTransform(angularVelocityError);
       constraintMomentSubSpace.transform(angularVelocityError);
 
-      positionError.scale(proportionalGains.getX(), proportionalGains.getY(), proportionalGains.getZ());
-      rotationError.scale(proportionalGains.getX(), proportionalGains.getY(), proportionalGains.getZ());
-      linearVelocityError.scale(derivativeGains.getX(), derivativeGains.getY(), derivativeGains.getZ());
-      angularVelocityError.scale(derivativeGains.getX(), derivativeGains.getY(), derivativeGains.getZ());
+      proportionalTermLinear.set(positionError);
+      proportionalTermLinear.scale(proportionalGains.getX(), proportionalGains.getY(), proportionalGains.getZ());
+      proportionalTermAngular.set(rotationError);
+      proportionalTermAngular.scale(proportionalGains.getX(), proportionalGains.getY(), proportionalGains.getZ());
+      derivativeTermLinear.set(linearVelocityError);
+      derivativeTermLinear.scale(derivativeGains.getX(), derivativeGains.getY(), derivativeGains.getZ());
+      derivativeTermAngular.set(angularVelocityError);
+      derivativeTermAngular.scale(derivativeGains.getX(), derivativeGains.getY(), derivativeGains.getZ());
 
-      forceA.add(positionError, linearVelocityError);
-      momentA.add(rotationError, angularVelocityError);
+      forceA.set(proportionalTermLinear);
+      forceA.add(derivativeTermLinear);
+      forceA.add(feedForwardForce);
+      momentA.set(proportionalTermAngular);
+      momentA.add(derivativeTermAngular);
+      momentA.add(feedForwardMoment);
 
       // Need to switch back to world before applying these.
       parentJoint.transformToNext.transform(forceA);
@@ -192,6 +216,16 @@ public class LoopClosureSoftConstraint
 
       constraintB.getYoForce().setAndNegate(forceA);
       constraintB.getYoMoment().setAndNegate(momentA);
+   }
+
+   /**
+    * Set the feed-forward force and moment to be applied at the constraint in addition to the error
+    * terms.
+    */
+   public void setFeedForward(Vector3DReadOnly force, Vector3DReadOnly moment)
+   {
+      constraintForceSubSpace.transform(force, feedForwardForce);
+      constraintMomentSubSpace.transform(moment, feedForwardMoment);
    }
 
    public String getName()
