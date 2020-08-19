66ab84f2a63cf7a8aede8f56bb9f8394efd85b56
Sylvain Bertrand <sbertrand@ihmc.us>
2020-08-19 13:04:03 -0500

    :bookmark: 0.20.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.20.1"
+   version = "0.20.2"
    vcsUrl = "https://github.com/ihmcrobotics/simulation-construction-set"
    openSource = true
    
