79d7f6476b93341c1e9fe8a7ebc4f1440378f224
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-18 17:15:55 -0600

    :arrow_up: ihmc-graphics-description 0.19.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ mainDependencies {
    api("us.ihmc:ihmc-commons:0.30.4")
    api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.4")
    api("us.ihmc:ihmc-robot-description:0.21.0")
-   api("us.ihmc:ihmc-graphics-description:0.19.2")
+   api("us.ihmc:ihmc-graphics-description:0.19.3")
    api("us.ihmc:ihmc-swing-plotting:0.19.2")
    api(ihmc.sourceSetProject("utilities"))
 }
