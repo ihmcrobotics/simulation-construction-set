d100a26fcd0d9175bfa0a2954cd0190fb9654a9b
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-18 17:31:41 -0600

    :arrow_up: ihmc-swing-plotting 0.19.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ mainDependencies {
    api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.4")
    api("us.ihmc:ihmc-robot-description:0.21.1")
    api("us.ihmc:ihmc-graphics-description:0.19.3")
-   api("us.ihmc:ihmc-swing-plotting:0.19.2")
+   api("us.ihmc:ihmc-swing-plotting:0.19.3")
    api(ihmc.sourceSetProject("utilities"))
 }
 
