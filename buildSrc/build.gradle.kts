ba01b3eb27d26f3a9de52f62187b4a794ccc4e3a
Duncan Calvert <dcalvert@ihmc.us>
2021-01-12 16:17:50 -0600

    Try and fix Bamboo.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,7 +2,7 @@ import com.gradle.publish.MavenCoordinates
 
 plugins {
    `java-gradle-plugin`
-   kotlin("jvm") version "1.3.41"
+   kotlin("jvm") version "1.4.10"
    id("com.gradle.plugin-publish") version "0.12.0"
 }
 
