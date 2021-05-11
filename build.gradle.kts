1a528d6fe60c480f80afa618c1d197ed252c3658
Duncan Calvert <dcalvert@ihmc.us>
2021-05-11 15:35:07 -0500

    :bookmark: 0.21.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.21.8"
+   version = "0.21.9"
    vcsUrl = "https://github.com/ihmcrobotics/simulation-construction-set"
    openSource = true
    
