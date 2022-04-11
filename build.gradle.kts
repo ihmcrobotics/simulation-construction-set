4e90f1b5ee66b70057aa2f6ca93932e6117ca1e6
Robert Griffin <rgriffin@ihmc.us>
2022-04-11 17:51:23 -0500

    :arrow_up: ihmc-yovariables 0.9.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ mainDependencies {
       exclude(group = "org.easytesting", module = "fest")
    }
 
-   api("us.ihmc:ihmc-yovariables:0.9.11")
+   api("us.ihmc:ihmc-yovariables:0.9.12")
    api("us.ihmc:ihmc-video-codecs:2.1.6")
    api("us.ihmc:euclid:0.17.1")
    api("us.ihmc:euclid-frame:0.17.1")
@@ -70,5 +70,5 @@ testDependencies {
 
 utilitiesDependencies {
    api("us.ihmc:euclid:0.17.1")
-   api("us.ihmc:ihmc-yovariables:0.9.11")
+   api("us.ihmc:ihmc-yovariables:0.9.12")
 }
