eb8cb58478c1658fef7df8abb933103370790815
Sylvain Bertrand <sbertrand@ihmc.org>
2022-06-29 15:54:29 -0500

    :arrow_up: ihmc-jmonkey-engine-toolkit 0.21.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ mainDependencies {
    api("us.ihmc:euclid-shape:0.17.2")
    api("us.ihmc:euclid-geometry:0.17.2")
    api("us.ihmc:ihmc-commons:0.31.0")
-   api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.21.0")
+   api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.21.1")
    api("us.ihmc:ihmc-robot-description:0.21.4")
    api("us.ihmc:ihmc-swing-plotting:0.19.5")
    api("us.ihmc:scs2-definition:0.8.5")
