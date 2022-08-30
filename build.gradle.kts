0adad5afc4a992aa8ff65a495a6b3b984b9c5ffb
Sylvain Bertrand <sbertrand@ihmc.org>
2022-08-30 11:32:51 -0500

    :arrow_up: scs2 17-0.9.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ mainDependencies {
    api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.21.2")
    api("us.ihmc:ihmc-robot-description:0.21.5")
    api("us.ihmc:ihmc-swing-plotting:0.19.6")
-   api("us.ihmc:scs2-definition:17-0.9.3")
+   api("us.ihmc:scs2-definition:17-0.9.4")
    api(ihmc.sourceSetProject("utilities"))
 }
 
